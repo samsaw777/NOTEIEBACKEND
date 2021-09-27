3b1e7b8a6cefe2c783caee1428683f892fef5942
Sameep <sameepsawant10@gmail.com>
2021-09-27 11:58:49 +0530

    fix:password reset link fixed

--- Routes/Forgotpassword.js
+++ Routes/Forgotpassword.js
@@ -4,15 +4,12 @@ const User = require("../Model/users");
 const crypto = require("crypto");
 require("dotenv").config();
 const nodeMailer = require("nodemailer");
-const sendGridTransporter = require("nodemailer-sendgrid-transport");
+// const sendGridTransporter = require("nodemailer-sendgrid-transport");
 
-const transporter = nodeMailer.createTransport(
-  sendGridTransporter({
-    auth: {
-      api_key: process.env.NODEMAILER_KEY,
-    },
-  })
-);
+const transporter = nodeMailer.createTransport({
+  service: "gmail",
+  auth: { user: "remotefirstworktracker@gmail.com", pass: "remote1234" },
+});
 
 //creating route to send the link to the user email
 router.post("/resetp", async (req, res) => {
@@ -29,13 +26,21 @@ router.post("/resetp", async (req, res) => {
     user.resetPasswordToken = token;
     user.tokenExpries = Date.now() + 3600000;
     user.save().then((response) => {
-      transporter.sendMail({
-        to: user.email,
-        from: "sameepsawant10@gmail.com",
-        subject: "Reset Password",
-        html: `<p>This mail is send to change ${user.name} password.</p> <h4>Click this <a href="https://remoteworktracker.netlify.app/reset/${token}">Link</a> to change this password.</h4>`,
-      });
-      res.json({ msg: "Check you message" });
+      transporter.sendMail(
+        {
+          to: user.email,
+          from: "sameepsawant10@gmail.com",
+          subject: "Reset Password",
+          html: `<p>This mail is send to change ${user.name} password.</p> <h4>Click this <a href="https://remoteworktracker.netlify.app/reset/${token}">Link</a> to change this password.</h4>`,
+        },
+        (err, response) => {
+          if (err) {
+            res.json(err);
+          } else {
+            res.json({ msg: "Check you message" });
+          }
+        }
+      );
     });
   });
 });
