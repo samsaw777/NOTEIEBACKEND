f510dad8973f306bea3310155d2a21673d42ccf6
Sameep <sameepsawant10@gmail.com>
2023-01-01 10:22:12 +0530

    cors added

--- server.js
+++ server.js
@@ -11,14 +11,7 @@ const MongodbUrl = process.env.DATABASE_URI;
 
 //This are middleware
 
-app.use(function (request, response, next) {
-  response.header("Access-Control-Allow-Origin", "*");
-  response.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+app.use(cors());
 app.use(express.json());
 // app.use(bodyParser.json());
 app.use("/", require("./Routes/routes"));
